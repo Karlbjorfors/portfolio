d5fbdc1932af9c52efd909672f752c7a89b82a1e
Kalle Björfors <kallebjorfors@live.co.uk>
2025-07-04 12:15:48 +0200

    Refactor Home component layout and add initial animation settings

--- app/page.jsx
+++ app/page.jsx
@@ -9,8 +9,12 @@ const Home = () => {
         opacity: 1,
         transition: { delay: 2.4, duration: 0.4, ease: "easeIn" }     
       }}
+      className='h-screen flex items-center'
     >
-      home
+      <div className='flex flex-col xl:flex-row items-center justify-between w-full'>
+        <div className='w-full xl:w-[550px]'>text</div>
+        <div className='hidden xl:block flex-1 relative z-20'>image</div>
+      </div>
     </motion.section>
   );
 };
