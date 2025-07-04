f173cbca0dbdc0bfd12441533a3ce15fd2da620c
Kalle Björfors <kallebjorfors@live.co.uk>
2025-07-04 09:29:58 +0200

    Refactor code structure for improved readability and maintainability

---
+++ components/NavLinks.jsx
@@ -0,0 +1,53 @@
+"use client";
+
+import { usePathname } from 'next/navigation';
+import React from 'react'
+import Link from 'next/link';
+
+const Links = [
+  {
+    name: "home",
+    path: "/"
+  },
+  {
+    name: "about",
+    path: "/about"
+  },
+  {
+    name: "services",
+    path: "/services"
+  },
+  {
+    name: "work",
+    path: "/work"
+  },
+  {
+    name: "contact",
+    path: "/contact"
+  }
+];
+
+const NavLinks = ({ containerStyles }) => {
+  const pathname = usePathname();
+  return (
+  <ul className={containerStyles}>
+    {Links.map((link, index) => {
+      // determine if the link matches the active route
+      const isActive = pathname === link.path;
+      // calculate the number of characters in the link name
+      const charLength = link.name.length;
+      // set the line width based on the character length
+      const lineWidth = charLength > 5 ? "after:w-[120%]" : "after:w-[90%]";
+      console.log(charLength );
+      return (
+        <Link href={link.path} key={index} className={`relative text-lg uppercase text-white ${isActive && `after:conent-[''] after:block after:absolute after:left-0 after:top-1/2 ${lineWidth} after:h-[4px] after:bg-accent after:-translate-y-1/2 after:z-0`}`}
+        >
+          <span className="relative z-10">{link.name}</span>
+        </Link>
+      );
+    })}
+    </ul>
+  );
+};
+
+export default NavLinks
