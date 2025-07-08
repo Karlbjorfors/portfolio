9e1d7b4dcac4602ce739ff39ece292e97d187e99
Kalle Björfors <kallebjorfors@live.co.uk>
2025-07-08 10:15:18 +0200

    Add Pattern component for background design in Home page

--- app/page.jsx
+++ app/page.jsx
@@ -8,6 +8,7 @@ import Blob from "@/components/Blob";
 import Image from "next/image";
 import avatarImg from "@/public/assets/avatar.png";
 import Socials from "@/components/Socials";
+import Pattern from "@/components/Pattern";
 
 const Home = () => {
   return (
@@ -19,6 +20,8 @@ const Home = () => {
       }}
       className="h-screen flex items-center"
     >
+      {/* pattern */}
+      <Pattern />
       <div className="flex flex-col xl:flex-row items-center justify-between w-full">
         {/* text */}
         <div className="w-full xl:w-[550px] flex flex-col items-center xl:items-start text-center xl:text-left">
