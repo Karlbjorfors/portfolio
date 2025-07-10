5f73f4af0fe3406230d083ed28b6df1c8950825c
Kalle Björfors <kallebjorfors@live.co.uk>
2025-07-10 15:42:54 +0200

    Add Skills and Tooltip components; enhance About page layout

--- app/about/page.jsx
+++ app/about/page.jsx
@@ -8,6 +8,9 @@ import Stats from "@/components/Stats";
 import Testimonial from "@/components/Testimonial";
 import Info from "@/components/Info";
 import Journey from "@/components/Journey";
+import Skills from "@/components/Skills";
+import Blob from "@/components/Blob";
+import Socials from "@/components/Socials";
 
 const About = () => {
   return (
@@ -22,8 +25,26 @@ const About = () => {
       <div className="container mx-auto px-0">
         <div className="flex flex-col xl:flex-row items-center gap-24 w-full h-[680px]">
           {/* img & social */}
-          <div className="hidden xl:flex w-full h-full pt-14 max-w-[430px] relative bg-pink-50/10">
-            1
+          <div className="hidden xl:flex flex-col w-full h-full pt-14 max-w-[430px] relative">
+            <Image
+              src="/assets/avatar.png"
+              width={320}
+              height={496}
+              alt=""
+              className="z-20 relative"
+            />
+            {/*  overlay */}
+            <div className="w-full h-[60px] absolute left-0 top-[420px] right-0 bg-gradient-to-t from-primary via-primary/90 z-30"></div>
+
+            {/* blob */}
+            <div className="absolute top-[80px] -left-[80px] z-10">
+              <Blob containerStyles="w-[420px] h-[420px] " />
+            </div>
+
+            <Socials
+              containerStyles="flex gap-4 z-40 w-max transform translate-x-[30px]"
+              iconStyles="w-[48px] h-[48px] text-[22px] text-accent hover:text-accent-hover transition-all flex items-center justify-center rounded-full cursor-pointer"
+            />
           </div>
           {/* scroll area */}
           <ScrollArea className="w-full h-[680px]">
@@ -35,7 +56,7 @@ const About = () => {
               <h2 className="h2 mb-6">
                 <span className="text-accent">Kalle</span> Björfors
               </h2>
-              <p className="max-w-[540px ] mb-12">
+              <p className="max-w-[540px] mb-12">
                 Lorem ipsum dolor sit amet, consectetur adipisicing elit.
                 Commodi ipsa ipsam rerum alias ab sapiente, facere esse a eum
                 doloremque aspernatur tempore distinctio assumenda dignissimos
@@ -46,7 +67,7 @@ const About = () => {
                 <Testimonial />
                 <Info />
                 <Journey />
-                <div>skills</div>
+                <Skills />
               </div>
             </div>
           </ScrollArea>
